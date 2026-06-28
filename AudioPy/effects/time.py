c60174871ec3fdac4ccde8e92d60875c484cc39d
Swarajhere <swaraj.nalawade2004@gmail.com>
2026-06-28 14:33:30 +0530

    removed previous project

--- AudioPy/effects/time.py
+++
@@ -1,7 +0,0 @@
-# audiopy/effects/time.py
-from scipy.signal import resample
-
-def time_stretch(samples, rate):
-    # rate >1 speeds up (shorter), <1 slows down (longer)
-    n_new = int(len(samples) / rate)
-    return resample(samples, n_new)
